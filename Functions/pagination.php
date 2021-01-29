36849e20e98f5330f4b43cba66fcfd75282c3785
Alex Coder <49398367+a1excoder@users.noreply.github.com>
2021-01-29 03:48:24 +0300

    Update pagination.php

--- Functions/pagination.php
+++ Functions/pagination.php
@@ -14,13 +14,11 @@ class Pagination
     public function getPagination($connect, $per_page, $from, $page)
     {
 
-
         $this->page = (int) $page;
         $total_count = $connect->query("SELECT COUNT(`id`) AS `total_count` FROM `{$from}`")->fetch_assoc()['total_count'];
         $this->total_pages = ceil($total_count / $per_page);
 
-        if ($this->page <= 1 || $this->page > $this->total_pages)
-        {
+        if ($this->page <= 1 || $this->page > $this->total_pages) {
             $this->page = 1;
         }
 
@@ -28,14 +26,11 @@ class Pagination
         $this->query = $connect->query("SELECT * FROM `{$from}` ORDER BY `id` DESC LIMIT {$offset}, {$per_page}");
 
 
-        for ($onePage = 1; $onePage <= $this->total_pages; $onePage++)
-        {
-            if ($onePage == $this->page)
-            {
+        for ($onePage = 1; $onePage <= $this->total_pages; $onePage++) {
+
+            if ($onePage == $this->page) {
                 $status = true;
-            }
-            else
-            {
+            } else {
                 $status = false;
             }
 
