9369d9afeba3c030931b8397320a2b471ade3073
Alex Coder <49398367+a1excoder@users.noreply.github.com>
2021-01-29 03:48:08 +0300

    Update main.php

--- main.php
+++ main.php
@@ -4,8 +4,8 @@ header('Content-type: application/json');
 include_once __DIR__ . '/core/pagination.php';
 include_once __DIR__ . '/core/Routes.php';
 
-use core\Routes as Route;
-use core\Pagination;
+use core\Routes as Router;
+use core\Pagination as Pagination;
 
 
 class index extends Pagination
@@ -20,12 +20,11 @@ class index extends Pagination
         $this->getPagination($connect, 6, 'posts', $page);
 
         $jsonArray = array();
-        $jsonArray[]['pagination'] = $this->pagination;
+        $jsonArray['pagination'] = $this->pagination;
 
 
-        while ($post = $this->query->fetch_assoc())
-        {
-            $jsonArray[] = [
+        while ($post = $this->query->fetch_assoc()) {
+            $jsonArray['posts'][] = [
                 'id' => (int) $post['id'],
                 'title' => $post['title'],
                 'category' => $post['category'],
@@ -45,16 +44,64 @@ class index extends Pagination
 
         $post = $connect->query("SELECT * FROM `posts` WHERE `id` = {$id}")->fetch_assoc();
 
-        $jsonArray['post'] = [
-            'post_id' => (int) $post['id'],
-            'datetime' => $post['datetime'],
-            'title' => $post['title'],
-            'category' => $post['category'],
-            'query' => $post['query']
-        ];
+        if (!$post) {
 
-        echo json_encode($jsonArray);
-        $connect->close();
+            http_response_code(404);
+            echo json_encode([
+                'status' => false,
+                'message' => 'Post not found'
+            ]);
+
+            $connect->close();
+        } else {
+
+            $jsonArray['post'] = [
+                'post_id' => (int) $post['id'],
+                'datetime' => $post['datetime'],
+                'title' => $post['title'],
+                'category' => $post['category'],
+                'query' => $post['query']
+            ];
+
+            echo json_encode($jsonArray);
+            $connect->close();
+        }
+
+    }
+
+
+    public function addPost()
+    {
+        $METHOD = $_SERVER['REQUEST_METHOD'];
+
+        if ($METHOD == 'POST') {
+            $connect = new mysqli($this->connect[0], $this->connect[1], $this->connect[2], $this->connect[3]);
+            $data = $_POST;
+
+            if (empty($data['title']) || empty($data['category']) || empty($data['query'])) {
+                http_response_code(406);
+                echo json_encode([
+                    'status' => false,
+                ]);
+
+            } else {
+                $connect->query("INSERT INTO `posts` (`id`, `title`, `datetime`, `category`, `query`) VALUES ".
+                    "(NULL, '{$data['title']}', current_timestamp(), '{$data['category']}', '{$data['query']}'); ");
+                $connect->close();
+
+                echo json_encode([
+                    'status' => true,
+                    'message' => 'New post has been created'
+                ]);
+            }
+
+        } else {
+            http_response_code(405);
+            echo json_encode([
+                'status' => false,
+                'message' => 'This is not a POST method'
+            ]);
+        }
 
     }
 
@@ -62,23 +109,25 @@ class index extends Pagination
 }
 
 
-
-Route::route('/', function ()
-{
+Router::route('/', function () {
     $new = new index();
     $new->viewPosts();
 });
 
-Route::route('/page/(\w+)', function (int $page)
-{
+Router::route('/post/new', function () {
+    $new = new index();
+    $new->addPost();
+});
+
+Router::route('/page/(\w+)', function (int $page) {
     $new = new index();
     $new->viewPosts($page);
 });
 
-Route::route('/post/(\w+)', function (int $id)
-{
+Router::route('/post/(\w+)', function (int $id) {
     $new = new index();
     $new->viewPost($id);
 });
 
-Route::execute($_SERVER['REQUEST_URI']);
+
+Router::execute($_SERVER['REQUEST_URI']);
