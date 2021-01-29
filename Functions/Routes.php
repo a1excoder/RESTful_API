7dbb94759a972aa54ae8a0539fa7b9155471a65f
Alex Coder <49398367+a1excoder@users.noreply.github.com>
2021-01-29 03:48:36 +0300

    Update Routes.php

--- Functions/Routes.php
+++ Functions/Routes.php
@@ -14,11 +14,9 @@ class Routes
 
     public static function execute($url)
     {
-        foreach (self::$routes as $pattern => $callback)
-        {
+        foreach (self::$routes as $pattern => $callback) {
 
-            if (preg_match($pattern, $url, $params))
-            {
+            if (preg_match($pattern, $url, $params)) {
                 array_shift($params);
                 return call_user_func_array($callback, array_values($params));
             }
